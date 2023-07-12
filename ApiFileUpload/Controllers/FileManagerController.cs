ab3836859bbb9e98a962f358f8fe38f762a1f67a
Tauhid Jafri <tauhidhussain@thekabadiwala.com>
2023-07-12 14:56:29 +0530

    Add project files.

---
+++ ApiFileUpload/Controllers/FileManagerController.cs
@@ -0,0 +1,33 @@
+﻿using ApiFileUpload.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiFileUpload.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileManagerController : ControllerBase
+    {
+        private readonly IManageImage _iManageImage;
+        public FileManagerController(IManageImage iManageImage)
+        {
+            _iManageImage = iManageImage;
+        }
+        [HttpPost]
+        [Route("uploadfile")]
+        public async Task<IActionResult> UploadFile(IFormFile _IFormFile)
+        {
+            var result = await _iManageImage.UploadFile(_IFormFile);
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("downloadfile")]
+        public async Task<IActionResult> DownloadFile(string FileName)
+        {
+            var result = await _iManageImage.DownloadFile(FileName);
+            return File(result.Item1, result.Item2, result.Item2);
+        }
+
+    }
+}
---
+++ ApiFileUpload/Program.cs
@@ -0,0 +1,21 @@
+using ApiFileUpload.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddTransient<IManageImage, ManageImage>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.MapControllers();
+
+app.Run();
